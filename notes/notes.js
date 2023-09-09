37e276e711cc9b1d540788a88cc5ce7b112ee4d8
Sizzlesina <www.sins604@gmail.com>
2023-09-09 09:51:44 +0330

    added a seperated notes file to the repo to take notes

---
+++ notes/notes.js
@@ -0,0 +1,37 @@
+// this file will be kept just to note any TIP and HINT
+
+/*
+before the react version 18 they were render the root like this :
+
+ React.render(<App />, document.getElementById("root")
+
+JSX is combined the CSS , HTML and JavaScript codes all together
+
+
+we can use the arrow functions in react like this:
+const test = () => {}
+
+
+in react we must keep a simple rule for all the functions:
+
+1. the function must start with a capital letter
+2. the function must return a make up
+
+
+Components in react contains the Data, Logic and Appearance all together 
+
+
+lets talk about the Declartive and Imperative type of codes:
+
+Imperative :
+  1. Manual DOM element selection and DOM traversing
+  2. Step by step DOM mulations until we reach the desired UI
+  HINT: it's about how to do things
+
+Declarative;
+  1. Describe what UI should look like using JSX based on current data 
+  2. React is an abstraction away from DOM: we never touch the DOM
+  3. Instead we think of the UI as a reflection of the current data
+  HINT: its about what we want
+  
+*/
