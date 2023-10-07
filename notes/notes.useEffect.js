3823a432d043af54171bc77f5beff0877e1520ef
Sizzlesina <www.sins604@gmail.com>
2023-10-07 18:19:08 +0330

    The useEffect dependency array

--- notes/notes.useEffect.js
+++ notes/notes.useEffect.js
@@ -28,4 +28,50 @@ A side effect is basically any "interaction between a React component and the wo
 
 ! These two will produce the same result,but at different moments.
 
+
+
+* Whats the useEffect dependency array?
+@ - By default, effects run after every render. We can prevent that by passing a dependency array
+
+@ - Without the dependency array, React doesn't know when to run the effect
+
+@ - Each time one of dependencies changes , the effect will be executed again
+
+@ - Every state variable and prop used inside the effect MUST be included in the dependency array
+! Otherwise we get a "stale closure". We will go more into depth in a future section 👉 
+
+
+
+* useEffect is a synchronization mechanism:
+@ - The mechanics of effects: 
+- useEffect is like an event listener that is listening for one dependency to change.Whenever a dependency changes,it will execute the effect again!
+
+
+- Effects react to updates to state and props used inside the effect (the dependencies). So effects are "reactive" (like state updates re-rendering the UI)
+
+++ title change / userRating changes => effect is executed again => document title is updated
+
+? Component state/ props => synchronize with => Exterrnal system (side effect)
+
+* Synchronization and lifecycle:
+@ - Dependency (state or props) changes => Effect is executed again and Component is re-redered 
+! Effects and component lifecycle are deeply connected
+
+- We can use the dependency array to run effects when the component renders or re-renders
+ 
+some examples for dependency array:
+
+@ - useEffect(fn, [x,y,z])
+* Synchronization => Effect synchronizes with x,y,z
+++ Lifecycle => Runs on mount and re-renders triggered by updating x,y or z
+
+@ - useEffect(fn,[]) 
+* Synchronization => Effect synchronizes with no state / props
+++ Lifecycle => Runs only on mount (initial render)
+
+@ - useEffect(fn)
+* Synchronization => Effect synchronizes with everything
+++ Lifecycle => Runs on every render (usually bad⛔)
+
+
 */
