2fb822042e39740ffe33c780ccb2a242a4d6953d
Sizzlesina <www.sins604@gmail.com>
2023-10-14 19:19:45 +0330

    Refs to select DOM elements

--- 07-usepopcorn/src/components/Search.js
+++ 07-usepopcorn/src/components/Search.js
@@ -1,15 +1,31 @@
 /** @format */
-import {useEffect} from 'react';
+import { useRef, useEffect } from "react";
 
-export default function Search({query,setQuery}) {
+export default function Search({ query, setQuery }) {
+  // useEffect(function(){
+  //   const el = document.querySelector(".search");
+  //   console.log(el);
+  //   el.focus();
+  //   // in React we wont select an element manually becasue it can cause us so many problems and instead we use "useRef"
+  // },[])
 
+  const inputElement = useRef(null);
 
-  useEffect(function(){
-    const el = document.querySelector(".search");
-    console.log(el);
-    el.focus();
-    // in React we wont select an element manually becasue it can cause us so many problems and instead we use "useRef"
-  },[])
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputElement.current) return;
+
+        if (e.code === "Enter") {
+          inputElement.current.focus();
+          setQuery("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
 
   return (
     <input
@@ -18,6 +34,7 @@ export default function Search({query,setQuery}) {
       placeholder='Search movies...'
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputElement}
     />
   );
 }
