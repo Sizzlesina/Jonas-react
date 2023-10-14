c4e71c96f41fb34256412a085c9c7cb9647ff094
Sizzlesina <www.sins604@gmail.com>
2023-10-14 09:40:10 +0330

    delayon the video

--- 07-usepopcorn/src/components/MovieDetails.js
+++ 07-usepopcorn/src/components/MovieDetails.js
@@ -31,7 +31,7 @@ export default function MovieDetails({
     Director: director,
     Genre: genre,
   } = movie;
-  
+
   // /* eslint-disable */
   // if (imdbRating > 8) [isTop, setIsTop] = useState(true);
   // if (imdbRating > 8) return <p>Greatest ever!</p>;
