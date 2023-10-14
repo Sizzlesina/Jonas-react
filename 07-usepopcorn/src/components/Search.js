0ca90df1be1060447841eb8b3d132696d60dbe0e
Sizzlesina <www.sins604@gmail.com>
2023-10-14 18:15:28 +0330

    how to NOT select DOM elements in React

--- 07-usepopcorn/src/components/Search.js
+++ 07-usepopcorn/src/components/Search.js
@@ -1,7 +1,15 @@
 /** @format */
+import {useEffect} from 'react';
+
+export default function Search({query,setQuery}) {
 
 
-export default function   Search({query,setQuery}) {
+  useEffect(function(){
+    const el = document.querySelector(".search");
+    console.log(el);
+    el.focus();
+    // in React we wont select an element manually becasue it can cause us so many problems and instead we use "useRef"
+  },[])
 
   return (
     <input
