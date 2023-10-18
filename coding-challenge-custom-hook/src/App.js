713f9a0a8da4d649d0eff01783971870fd3473ea
Sizzlesina <www.sins604@gmail.com>
2023-10-18 19:48:06 +0330

    Coding Challenge

---
+++ coding-challenge-custom-hook/src/App.js
@@ -0,0 +1,44 @@
+/** @format */
+
+import { useState } from "react";
+import { useGeoLocation } from "./useGeoLocation";
+
+export default function App() {
+  const {
+    isLoading,
+    position: { lat, lng },
+    getPosition,
+    error,
+  } = useGeoLocation();
+
+  const [countClicks, setCountClicks] = useState(0);
+
+  function handleClick() {
+    setCountClicks((count) => count + 1);
+    getPosition();
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick} disabled={isLoading}>
+        Get my position
+      </button>
+
+      {isLoading && <p>Loading position...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && lat && lng && (
+        <p>
+          Your GPS position:{" "}
+          <a
+            target='_blank'
+            rel='noreferrer'
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}>
+            {lat}, {lng}
+          </a>
+        </p>
+      )}
+
+      <p>You requested position {countClicks} times</p>
+    </div>
+  );
+}
---
+++ coding-challenge-custom-hook/src/useGeoLocation.js
@@ -0,0 +1,32 @@
+/** @format */
+
+import { useState } from "react";
+
+export function useGeoLocation() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
+
+  function getPosition() {
+    // const { lat, lng } = position;
+    if (!navigator.geolocation)
+      return setError("Your browser does not support geolocation");
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
+
+  return { isLoading, position, getPosition, error };
+}
