9653d74d22dd60d93badedef955263250cdcf692
Sizzlesina <www.sins604@gmail.com>
2023-10-21 11:11:44 +0330

    Starting the classy weather app

---
+++ 09-classy-weather/src/components/AppV2.js
@@ -0,0 +1,36 @@
+/** @format */
+
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: "lisbon",
+    };
+    this.fetchWeather = this.fetchWeather.bind(this);
+  }
+  fetchWeather() {
+    console.log("Loading data...");
+    console.log(this);
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <h1>Classy Weather</h1>
+        <div>
+          <input
+            type='text'
+            placeholder='Search for location...'
+            value={this.state.location}
+            onChange={(e) => this.setState({ location: e.target.value })}
+          />
+        </div>
+        <button onClick={this.fetchWeather}>Get Weather</button>
+      </div>
+    );
+  }
+}
+
+export default App;
--- 09-classy-weather/src/index.js
+++ 09-classy-weather/src/index.js
@@ -2,13 +2,13 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import AppV2 from "./components/AppV2";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <AppV2 />
   </React.StrictMode>
 );
