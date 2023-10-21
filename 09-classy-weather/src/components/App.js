687b53bf7994d7c8ebb8a43aa031613eb62d2050
Sizzlesina <www.sins604@gmail.com>
2023-10-21 18:22:06 +0330

    Lifecycle methods: end of the section

--- 09-classy-weather/src/components/App.js
+++ 09-classy-weather/src/components/App.js
@@ -35,7 +35,7 @@ function formatDay(dateStr) {
 
 class App extends React.Component {
   state = {
-    location: "lisbon",
+    location: "",
     isLoading: false,
     displayLocation: "",
     weather: {},
@@ -43,6 +43,8 @@ class App extends React.Component {
 
   // async fetchWeather() {
   fetchWeather = async () => {
+    if (this.state.location.length < 2) return this.setState({ weather: {} });
+
     try {
       this.setState({ isLoading: true });
       // 1) Getting location (geocoding)
@@ -67,13 +69,28 @@ class App extends React.Component {
       const weatherData = await weatherRes.json();
       this.setState({ weather: weatherData.daily });
     } catch (err) {
-      console.err(err);
+      console.error(err);
     } finally {
       this.setState({ isLoading: false });
     }
   };
 
   setLocation = (e) => this.setState({ location: e.target.value });
+
+  componentDidMount() {
+    // this.fetchWeather();
+
+    this.setState({ location: localStorage.getItem("location") || "" });
+  }
+
+  // useEffect [location]
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.location !== prevState.location) {
+      this.fetchWeather();
+
+      localStorage.setItem("location", this.state.location);
+    }
+  }
 
   render() {
     return (
@@ -83,7 +100,6 @@ class App extends React.Component {
           location={this.state.location}
           onChangeLocation={this.setLocation}
         />
-        <button onClick={this.fetchWeather}>Get Weather</button>
 
         {this.state.isLoading && <p className='loader'>Loading...</p>}
         {this.state.weather.weathercode && (
@@ -94,7 +110,7 @@ class App extends React.Component {
         )}
       </div>
     );
-  }       
+  }
 }
 
 export default App;
@@ -115,6 +131,10 @@ class Input extends React.Component {
 }
 
 class Weather extends React.Component {
+  componentWillUnmount() {
+    console.log("weather will unmount");
+  }
+
   render() {
     const {
       temperature_2m_max: max,
@@ -157,5 +177,3 @@ class Day extends React.Component {
     );
   }
 }
-
-// Last commit 
