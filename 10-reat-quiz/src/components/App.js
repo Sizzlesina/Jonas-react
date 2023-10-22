f80c80ea31938d2577bf956caa95bf71377e1234
Sizzlesina <www.sins604@gmail.com>
2023-10-22 20:16:37 +0330

    Handling loading,error and ready status

--- 10-reat-quiz/src/components/App.js
+++ 10-reat-quiz/src/components/App.js
@@ -2,6 +2,9 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
   questions: [],
@@ -29,7 +32,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
   useEffect(function () {
     fetch(`http://localhost:9000/questions`)
       .then((res) => res.json())
@@ -41,8 +47,9 @@ export default function App() {
       <Header />
 
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
