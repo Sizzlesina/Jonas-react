3aa975e73a152c7b6303a12e72f32ab711f521b5
Sizzlesina <www.sins604@gmail.com>
2023-10-23 18:32:48 +0330

    Displaying progress

--- 10-reat-quiz/src/components/App.js
+++ 10-reat-quiz/src/components/App.js
@@ -7,6 +7,7 @@ import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
+import Progress from "./Progress";
 
 const initialState = {
   questions: [],
@@ -52,12 +53,16 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
 
   useEffect(function () {
     fetch(`http://localhost:9000/questions`)
@@ -77,6 +82,12 @@ export default function App() {
         )}
         {status === "active" && (
           <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer} />
             <Question
               questions={questions[index]}
               dispatch={dispatch}
