97a9f5942f94e14c1355cd71e22a840156f1af71
Sizzlesina <www.sins604@gmail.com>
2023-10-25 19:02:55 +0330

    Notes

---
+++ notes/notes-worldwide.js
@@ -0,0 +1,27 @@
+/*
+* what is routing?
+@ Routing:
+- With routing, we match different URLs to diffrent UI views (React components): routes
+
+- This enables users to navigate between different applications screens, using the browser URL
+
+- Keeps the UI in sync with the current browser URL
+
+- routing in React will be used with React router
+
+- Allow us to build single-page applications
+
+! HINT: Routing in React is client side
+
+* single page applications (SPA):
+@ - APplications that is executed entirely on the client (browsers)
+
+@ - Routes: different URls correspond to different views (components)
+
+@ - The page is never reloaded 
+
+@ - Feels like a native app
+
+@ - user clicked router link => URL is changed => DOM is updated : React component corresponding to the new URL is rendered
+
+*/
