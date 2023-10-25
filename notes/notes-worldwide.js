154b634d11345a6fa87b960a6445889affa9bbb6
Sizzlesina <www.sins604@gmail.com>
2023-10-25 19:55:52 +0330

    Vite commands

--- notes/notes-worldwide.js
+++ notes/notes-worldwide.js
@@ -24,4 +24,26 @@
 
 @ - user clicked router link => URL is changed => DOM is updated : React component corresponding to the new URL is rendered
 
+
+ * Creating react app:
+1 - npm create vite@4
+2 - npm install
+3 - npm run dev
+* Creating eslint file:
+1 - npm install eslint vite-plugin-eslint eslint-config-react-app --save-dev
+
+2 - create a eslintrc.json file
+
+3 - write this piece of code => {"extends": "react-app" }
+
+4 - in the vite.config.js : 
+
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
+
+export default defineConfig({
+  plugins: [react(),eslint()],
+});
+
 */
