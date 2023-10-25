9674e32478dda88d5239525878bde76adf59a2b0
Sizzlesina <www.sins604@gmail.com>
2023-10-25 09:10:34 +0330

    line removed

--- 10-reat-quiz/src/components/FinishScreen.js
+++ 10-reat-quiz/src/components/FinishScreen.js
@@ -12,7 +12,7 @@ function FinishScreen({ points, maxPossiblePoints, highscore }) {
   return (
     <>
       <p className='result'>
-        <span>{emoji}</span>You scored <strong>{points}</strong> out of{" "}
+        <span>{emoji}</span>You scored <strong>{points}</strong> out of
         {maxPossiblePoints} ({Math.ceil(percentage)}%)
       </p>
       <p className='highscore'>(Highscore: {highscore} points)</p>
