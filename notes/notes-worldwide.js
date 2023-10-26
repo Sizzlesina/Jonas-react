b5ce3da42889d3f4db72ba97e43a1bd7741614ea
Sizzlesina <www.sins604@gmail.com>
2023-10-27 00:38:16 +0330

    Storing state in the URL

--- notes/notes-worldwide.js
+++ notes/notes-worldwide.js
@@ -86,4 +86,25 @@ for example:
 "server": "json-server --watch data/cities.json --port 9000 --delay 500"
 3 - npm run server 
 
+
+* The URL for state management:
+@ - The URL is an excellent place to store UI state and an alternative to useState in some situations! 
+Examples: open/closed panels, currently selected list item,list sorting order,applied list filters
+
+++ Why we do that?
+1 - Easy way to store in a global place,accssible to all components in the app
+2 - Good way to "pass" data from one page into the next page
+3 - Makes it possible to bookmark and share the page with the exact UI state it had at the time
+ 
+* How we can do this using React Router?
+@ tools:
+www.example.com/app/cities/lisbon?lat=38.728&lng=-9.141
+++ Path: app/cities
+++ Params: lisbon
+++ Query string: lat=38.728&lng=-9.141
+
+* Lets look at this example in more detail:
+@ - City name and GPS Location were retrieved from the URL instead of application state
+++ lisbon => city name
+++ lat=38.728&lng=-9.141 => GPS Location
 */
