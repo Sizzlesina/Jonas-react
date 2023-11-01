2242160e1e8f616dcfa77310d89908793595c837
Sizzlesina <www.sins604@gmail.com>
2023-11-01 19:49:56 +0330

    Fetching city data in the form

--- 11-world-wise/src/components/Map.jsx
+++ 11-world-wise/src/components/Map.jsx
@@ -14,19 +14,17 @@ import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 import { useGeolocation } from "../hooks/useGeoLocation";
 import Button from "./Button";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 
 function Map() {
   const { cities } = useCities();
-  const [mapPosition, setMapPosition] = useState([40, 0]);
-  const [searchParams] = useSearchParams();
+  const [mapPosition, setMapPosition] = useState([100,2000]);
   const {
     isLoading: isLoadingPosition,
     position: geoLocationPosition,
     getPosition,
   } = useGeolocation();
-
-  const mapLat = searchParams.get("lat");
-  const mapLng = searchParams.get("lng");
+  const [mapLat, mapLng] = useUrlPosition();
 
   useEffect(
     function () {
