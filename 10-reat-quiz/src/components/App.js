6d5d4de7a11ebc4810355e43e3a0f8b56ac43654
Sizzlesina <www.sins604@gmail.com>
2023-11-08 19:16:21 +0330

    Small problem solved

--- 10-reat-quiz/src/components/App.js
+++ 10-reat-quiz/src/components/App.js
@@ -14,28 +14,27 @@ import Timer from "./Timer";
 import { useQuiz } from "../context/QuizContext";
 
 export default function App() {
-  const { status} = useQuiz();
+  const { status } = useQuiz();
 
-  
   return (
     <div className='app'>
       <Header />
-=        <Main>
-          {status === "loading" && <Loader />}
-          {status === "error" && <Error />}
-          {status === "ready" && <StartScreen />}
-          {status === "active" && (
-            <>
-              <Progress />
-              <Question />
-              <Footer>
-                <Timer />
-                <NextButton />
-              </Footer>
-            </>
-          )}
-          {status === "finished" && <FinishScreen />}
-        </Main>
-=    </div>
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen />}
+        {status === "active" && (
+          <>
+            <Progress />
+            <Question />
+            <Footer>
+              <Timer />
+              <NextButton />
+            </Footer>
+          </>
+        )}
+        {status === "finished" && <FinishScreen />}
+      </Main>
+    </div>
   );
 }
