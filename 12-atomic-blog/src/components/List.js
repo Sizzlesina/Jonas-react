655732c951013dbcf7ba460d5ab24c89cc9237b9
Sizzlesina <www.sins604@gmail.com>
2023-11-08 20:51:52 +0330

    A surprising optimization trick with children

--- 12-atomic-blog/src/components/List.js
+++ 12-atomic-blog/src/components/List.js
@@ -1,19 +1,24 @@
 /** @format */
 
 import { usePosts } from "./PostContext";
+import Test from "./Test";
 
 function List() {
   const { posts } = usePosts();
 
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+
+      {/* <Test /> */}
+    </>
   );
 }
 export default List;
