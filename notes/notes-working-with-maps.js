a980aac5663853c4cff2e66a9d1d2e68ba079754
Sizzlesina <www.sins604@gmail.com>
2023-11-11 19:45:15 +0330

    All about map services

---
+++ notes/notes-working-with-maps.js
@@ -0,0 +1,29 @@
+/*
+* Leaflet:
+1 - npm install leaflet react-leaflet
+2 - import Map , TileLayer m Marker , Popup and other components from the react-leaflet
+3 - customizing the components
+
+
+* Neshan:
+1 - npm install @neshan-maps-platform/react-openlayers --save
+2 - import NeshanMap from "@neshan-maps-platform/react-openlayers"
+import "@neshan-maps-platform/react-openlayers/dist/style.css"
+
+3 - function App() {
+ return <NeshanMap
+       mapKey="YOUR_MAP_KEY"
+></NeshanMap>
+}
+
+! HINT: this service is have a problem which is: it doesnt have any zoom in and zoom out button and we cant mark an location in it but it supports openLayer and we can add them using the openlayer
+
+
+* OpenLayer:
+1 - npm create ol-app my-app
+2 - cd my-app
+3 - npm start
+4 - if we need to build the app we using npm build
+
+
+*/
