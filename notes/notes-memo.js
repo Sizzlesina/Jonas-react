68505afa2b54664ea209a71b235b5b23b6e491a9
Sizzlesina <www.sins604@gmail.com>
2023-11-15 17:37:11 +0330

    Notes

--- notes/notes-memo.js
+++ notes/notes-memo.js
@@ -115,7 +115,13 @@
 
 
 * The bundle and code splitting:
-@ - Bundle: JavaScript file containing the entire application code. Downloading the bundle will laod the entire app at once, turnin it into a SPA
+@ - Bundle: JavaScript file containing the entire application code. Downloading the bundle will laod the entire app at once, turnin it into a SPA 
+(Produce by a tool like webpack (inside create -react-app) or vite)
 
- 
+@ - Bundle size: Amount of JavaScript users have to download to start using the app.One of the most important things to be optimized, so that the bundle takes less time to download
+
+@ - Code splitting: Splitting bundle into multiple parts that can be downloaded over time ("lazy loading")
+
+! SPA = single page applications
+
 */
