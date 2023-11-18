309037018e9dc5afe05f4968d2542f4b6984eae1
Sizzlesina <www.sins604@gmail.com>
2023-11-18 12:56:41 +0330

    Creating a Redux store

--- 14-redux-intro/src/index.js
+++ 14-redux-intro/src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
-import "./index.css"
+/** @format */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import "./index.css";
+import "./store";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    < App />
+    <App />
   </React.StrictMode>
-)
+);
--- 14-redux-intro/src/store.js
+++ 14-redux-intro/src/store.js
@@ -1,4 +1,6 @@
 /** @format */
+import { createStore } from "redux";
+// Depricated way
 
 const initialState = {
   balance: 0,
@@ -15,7 +17,11 @@ function reducer(state = initialState, action) {
     case "account/requestLoan":
       if (state.loan > 0) return state;
       // LATER
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+      };
     case "account/payLoan":
       return {
         ...state,
@@ -27,3 +33,23 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+store.dispatch({ type: "account/deposit", payload: 500 });
+console.log(store.getState());
+
+store.dispatch({ type: "account/withdraw", payload: 200 });
+console.log(store.getState());
+
+store.dispatch({
+  type: "account/requestLoan",
+  payload: {
+    amount: 100,
+    purpose: "Buy a car",
+  },
+});
+console.log(store.getState());
+
+store.dispatch({ type: "account/payLoan" });
+console.log(store.getState());
