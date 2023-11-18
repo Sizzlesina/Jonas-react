8114b43eddb60456a4e7b00c27af8d9a793542e7
Sizzlesina <www.sins604@gmail.com>
2023-11-18 09:55:40 +0330

    Setting state based on other state updates

--- 13-workout-timer/src/Calculator.js
+++ 13-workout-timer/src/Calculator.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import { memo, useEffect, useState } from "react";
+import { memo, useCallback, useEffect, useState } from "react";
 import clickSound from "./ClickSound.m4a";
 
 function Calculator({ workouts, allowSound }) {
@@ -11,28 +11,49 @@ function Calculator({ workouts, allowSound }) {
 
   const [duration, setDuration] = useState(0);
 
+  // const playSound = useCallback(
+  //   function () {
+  //     if (!allowSound) return;
+  //     const sound = new Audio(clickSound);
+  //     sound.play();
+  //   },
+  //   [allowSound]
+  // );
+
   useEffect(
     function () {
       setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+
+      // playSound();
     },
     [number, sets, speed, durationBreak]
+  );
+
+  useEffect(
+    function () {
+      const playSound =  function () {
+        if (!allowSound) return;
+        const sound = new Audio(clickSound);
+        sound.play();
+      };
+
+      playSound();
+    },
+
+    [duration,allowSound]
   );
 
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
-  const playSound = function () {
-    if (!allowSound) return;
-    const sound = new Audio(clickSound);
-    sound.play();
-  };
-
   function handleInc() {
     setDuration((duration) => Math.floor(duration) + 1);
+    // playSound();
   }
 
   function handleDec() {
     setDuration((duration) => (duration > 0 ? Math.ceil(duration) - 1 : 0));
+    // playSound();
   }
 
   return (
