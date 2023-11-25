1a8e2dd96b3702f616ac67a111cc5d2e71e2b2a9
Sizzlesina <www.sins604@gmail.com>
2023-11-25 12:00:40 +0330

    Fast React Pizza project

---
+++ 15-fast-react-pizza/src/App.jsx
@@ -0,0 +1,7 @@
+/** @format */
+
+function App() {
+  return <div>App</div>;
+}
+
+export default App;
--- 15-fast-react-pizza/vite.config.js
+++ 15-fast-react-pizza/vite.config.js
@@ -1,10 +1,7 @@
-/** @format */
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
-});
+  plugins: [react(),eslint()],
+})
