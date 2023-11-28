dd0de9bcc380c733341e5235b3783efdf96fda24
Sizzlesina <www.sins604@gmail.com>
2023-11-28 18:07:55 +0330

    Fetching orders

--- 15-fast-react-pizza/src/App.jsx
+++ 15-fast-react-pizza/src/App.jsx
@@ -6,7 +6,7 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
       },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
-      { path: "/order/:orderId", element: <Order /> },
+      { path: "/order/:orderId", element: <Order />, loader: orderLoader },
     ],
   },
 ]);
--- 15-fast-react-pizza/src/ui/Header.jsx
+++ 15-fast-react-pizza/src/ui/Header.jsx
@@ -1,13 +1,18 @@
-import { Link } from "react-router-dom"
+/** @format */
+
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
-  
   return (
     <header>
       <Link to='/'>Fast React Pizza Co.</Link>
-      <p>Jonas</p>  
+
+      <SearchOrder />
+
+      <p>Jonas</p>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
