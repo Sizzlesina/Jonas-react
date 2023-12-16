1788df4c21d72fd463fa46d2c5039b6556fc1ff4
Sizzlesina <www.sins604@gmail.com>
2023-12-16 10:12:08 +0330

    Styling form elements

--- 15-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 15-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -65,12 +65,18 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input
+              type="text"
+              name="address"
+              required
+              className="placeholder:tex-stone-400 w-full rounded-full border border-stone-200 px-3 py-2 text-sm transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3 "
+            />
           </div>
         </div>
 
         <div>
           <input
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2 "
             type="checkbox"
             name="priority"
             id="priority"
--- 15-fast-react-pizza/src/features/order/SearchOrder.jsx
+++ 15-fast-react-pizza/src/features/order/SearchOrder.jsx
@@ -1,25 +1,26 @@
 /** @format */
 
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function SearchOrder() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
   const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
     if (!query) return;
     navigate(`/order/${query}`);
-    setQuery("");
+    setQuery('');
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <input
-        placeholder='Search order #'
+        placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72 "
       />
     </form>
   );
