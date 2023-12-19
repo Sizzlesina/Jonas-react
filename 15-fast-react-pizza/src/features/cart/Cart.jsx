39c1c75384b693c5c281fe33fed7e0b08e386a7e
Sizzlesina <www.sins604@gmail.com>
2023-12-19 20:14:37 +0330

    Building the cart page

--- 15-fast-react-pizza/src/features/cart/Cart.jsx
+++ 15-fast-react-pizza/src/features/cart/Cart.jsx
@@ -1,7 +1,10 @@
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart, getCart } from './cartSlice';
+import EmptyCart from './EmptyCart';
+
 
 const fakeCart = [
   {
@@ -29,7 +32,10 @@ const fakeCart = [
 
 function Cart() {
   const username = useSelector((state) => state.user.username);
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
+  const dispatch = useDispatch();
+
+  if(!cart.length) return <EmptyCart />
 
   return (
     <div className="px-4 py-3">
@@ -47,7 +53,9 @@ function Cart() {
         <Button type="primary" to="/order/new">
           Order Pizzas
         </Button>
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={() => dispatch(clearCart())}>
+          Clear cart
+        </Button>
       </div>
     </div>
   );
--- 15-fast-react-pizza/src/features/cart/CartItem.jsx
+++ 15-fast-react-pizza/src/features/cart/CartItem.jsx
@@ -1,9 +1,15 @@
+import { useDispatch } from 'react-redux';
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
+import { deleteItem } from './cartSlice';
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
+  const dispatch = useDispatch();
 
+  function handleDeleteItem(){
+    dispatch(deleteItem())
+  }
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between sm:gap-6">
       <p className="mb-1 sm:mb-0">
@@ -11,7 +17,7 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small">Delete</Button>
+        <Button type="small" onClick={handleDeleteItem}>Delete</Button>
       </div>
     </li>
   );
