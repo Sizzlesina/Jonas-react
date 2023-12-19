e88e4da07c7420e87adc6e258d7d439dbd85cd69
Sizzlesina <www.sins604@gmail.com>
2023-12-19 20:33:38 +0330

    Deleting Cart Items

--- 15-fast-react-pizza/src/features/cart/CartItem.jsx
+++ 15-fast-react-pizza/src/features/cart/CartItem.jsx
@@ -1,23 +1,18 @@
-import { useDispatch } from 'react-redux';
-import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
-import { deleteItem } from './cartSlice';
+import DeleteItem from './DeleteItem';
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
-  const dispatch = useDispatch();
 
-  function handleDeleteItem(){
-    dispatch(deleteItem())
-  }
   return (
-    <li className="py-3 sm:flex sm:items-center sm:justify-between sm:gap-6">
+    <li className="py-3 sm:flex sm:items-center sm:justify-between ">
       <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
-      <div className="flex items-center justify-between">
-        <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small" onClick={handleDeleteItem}>Delete</Button>
+      <div className="flex items-center justify-between sm:gap-6">
+        <p className="text-sm font-bold">{formatCurrency(totalPrice)} </p>
+
+        <DeleteItem pizzaId={pizzaId}/>
       </div>
     </li>
   );
