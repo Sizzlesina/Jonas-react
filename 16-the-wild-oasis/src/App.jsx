fd128df74f8aa08f97440752a0c4e4078526fcb6
Sizzlesina <www.sins604@gmail.com>
2023-12-31 18:43:11 +0330

    Fetching Cabin data

--- 16-the-wild-oasis/src/App.jsx
+++ 16-the-wild-oasis/src/App.jsx
@@ -17,7 +17,7 @@ import AppLayout from "./ui/AppLayout";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 60 * 1000,
+      staleTime: 0,
     },
   },
 });
