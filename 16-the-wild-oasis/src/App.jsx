7ca3b6e5d2a9b5736aa2c41e74a3aa766af8c848
Sizzlesina <www.sins604@gmail.com>
2024-01-02 18:13:17 +0330

    Displaying toasts(notification)

--- 16-the-wild-oasis/src/App.jsx
+++ 16-the-wild-oasis/src/App.jsx
@@ -13,6 +13,7 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyled";
 import AppLayout from "./ui/AppLayout";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -24,25 +25,47 @@ const queryClient = new QueryClient({
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<Navigate replace to='dashboard' />} />
-            <Route path='dashboard' element={<Dashboard />} />
-            <Route path='bookings' element={<Bookings />} />
-            <Route path='cabins' element={<Cabins />} />
-            <Route path='users' element={<Users />} />
-            <Route path='settings' element={<Settings />} />
-            <Route path='account' element={<Account />} />
-          </Route>
-          <Route path='login' element={<Login />} />
-          <Route path='*' element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <GlobalStyles />
+        <BrowserRouter>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route index element={<Navigate replace to='dashboard' />} />
+              <Route path='dashboard' element={<Dashboard />} />
+              <Route path='bookings' element={<Bookings />} />
+              <Route path='cabins' element={<Cabins />} />
+              <Route path='users' element={<Users />} />
+              <Route path='settings' element={<Settings />} />
+              <Route path='account' element={<Account />} />
+            </Route>
+            <Route path='login' element={<Login />} />
+            <Route path='*' element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+      <Toaster
+        position='top-center'
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          susccess: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
+    </>
   );
 }
 
--- 16-the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 16-the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -58,13 +59,15 @@ function CabinRow({ cabin }) {
   const { isDeleting, mutate } = useMutation({
     mutationFn: (id) => deleteCabin(id),
     onSuccess: () => {
-      alert("Cabin successfully deleted");
       queryClient.invalidateQueries({
-        queryKey: ["cabin"],
+        queryKey: ["cabins"],
       });
+      toast.success("Cabin successfully deleted");
+
     },
-    onError: (err) => alert(err.message),
+    onError: (err) => toast.error(err.message),
   });
+
   return (
     <TableRow role='row'>
       <Img src={image} />
