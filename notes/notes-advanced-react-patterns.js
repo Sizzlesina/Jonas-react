041c4ff65a5a410795a83a9faade031a81b80f23
Sizzlesina <www.sins604@gmail.com>
2024-01-06 12:59:05 +0330

    The render props pattern

--- notes/notes-advanced-react-patterns.js
+++ notes/notes-advanced-react-patterns.js
@@ -5,4 +5,19 @@
 * Compound component pattern:
 - For very self-contained components that need/want to manage their own state. Compound components are like fancy super-components
 
+
+* Faker API:
+- We can use this fake api to test the application
+@ How to use it?
+1- npm i @faker-js/faker
+2- import {faker} from '@faker-js/faker'
+3- const variable = Array.from({length : (number), () =>{
+  return {
+    (another name) : faker.(a name (read the document).(something))
+  }
+}})
+
+* Render prop
+- A prop that we will give to the component that we want to render in case to (idk check that later)
+
 */
