e9d58ad4e4f1598a215071ed9e793bd11b1d6508
Sizzlesina <www.sins604@gmail.com>
2024-01-06 12:16:16 +0330

    Advanced React patterns

---
+++ notes/notes-advanced-react-patterns.js
@@ -0,0 +1,8 @@
+/*
+* Render prop pattern:
+- For complete control over what the component renders,by passing in a function that tells the component what to render.Was more common before hooks,but still useful
+
+* Compound component pattern:
+- For very self-contained components that need/want to manage their own state. Compound components are like fancy super-components
+
+*/
