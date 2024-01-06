9214e623afdc11d93446ffebad7e60ee568f286b
Sizzlesina <www.sins604@gmail.com>
2024-01-06 12:02:43 +0330

    Updating application setting

---
+++ 16-the-wild-oasis/src/features/settings/useUpdateSetting.js
@@ -0,0 +1,20 @@
+/** @format */
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { updateSetting as updateSettingApi } from "../../services/apiSettings";
+
+export function useUpdateSetting() {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateSetting, isLoading: isUpdating } = useMutation({
+    mutationFn: updateSettingApi,
+    onSuccess: () => {
+      toast.success("Cabin successfully edited");
+      queryClient.invalidateQueries({ queryKey: ["settings"] });
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { isUpdating, updateSetting };
+}
